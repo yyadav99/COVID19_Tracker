eb6a57996a8b32493c047b8637edd0177c30b13d
Yogesh Yadav <yadavyogesh9999@gmail.com>
2022-05-28 00:51:41 +0530

    Updates

---
+++ precache-manifest.917cfc6233077654d6bd5c5a97205a75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb60e72f1928b4852a97f144c507af9e",
+    "url": "/COVID19_Tracker/index.html"
+  },
+  {
+    "revision": "889d3640f937f6b6864f",
+    "url": "/COVID19_Tracker/static/css/main.e8ba67b7.chunk.css"
+  },
+  {
+    "revision": "33792af25fd12554c07d",
+    "url": "/COVID19_Tracker/static/js/2.c1e9156f.chunk.js"
+  },
+  {
+    "revision": "5e5c5aaefd726bfc2ad51722d14c5d57",
+    "url": "/COVID19_Tracker/static/js/2.c1e9156f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "889d3640f937f6b6864f",
+    "url": "/COVID19_Tracker/static/js/main.6c740caa.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/COVID19_Tracker/static/js/main.6c740caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cb406450e329378ef74",
+    "url": "/COVID19_Tracker/static/js/runtime-main.9f302951.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID19_Tracker/static/media/image.d7265326.png"
+  }
+]);
